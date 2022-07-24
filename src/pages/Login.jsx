92067446470f38e1edc19d078402fbb49873f95a
Tharindu <tharinduu743@gmail.com>
2022-07-24 10:48:28 +0530

    Setup routing and material ui

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login pAGE
+
+      <Button variant="contained">Login</Button>
+    </div>
+  )
+}
+
+export default Login
