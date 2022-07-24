a84e5c4e81981832fcf352b1116d0632c86616a4
Tharindu <tharinduu743@gmail.com>
2022-07-25 00:20:18 +0530

    add login page ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,81 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { TextField, Card, Container, Button } from "@mui/material";
+import React, { useState } from "react";
+
+const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [hasError, setError] = useState(false);
+
+  const [password, setPassword] = useState("");
+
+  const handleOnLogin = () => {
+    // TODO: send email and the password to the apis
+  };
+
+  const handleOnChangeEmail = (event) => {
+    setEmail(event.target.value);
+
+    // validate email
+    if (emailRegex.test(event.target.value)) {
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
+  const handleOnChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div>Login pAGE
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card
+        sx={{
+          p: 4,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          error={hasError}
+          onChange={handleOnChangeEmail}
+          sx={{
+            minWidth: "400px",
+            mb: 2,
+          }}
+          label="Email"
+          variant="outlined"
+        />
+        <TextField
+          onChange={handleOnChangePassword}
+          sx={{
+            minWidth: "400px",
+            mb: 2,
+          }}
+          label="Password"
+          variant="outlined"
+        />
+        <Button
+          disabled={!email || !password || hasError}
+          onClick={handleOnLogin}
+          sx={{
+            minWidth: "400px",
+          }}
+          variant="contained"
+        >
+          Login
+        </Button>
+      </Card>
+    </Container>
+  );
+};
 
-      <Button variant="contained">Login</Button>
-    </div>
-  )
-}
-
-export default Login
+export default Login;
